b1c7d82b96a7748d19b7dad72df6b732f32dc9a0
Nshandeshu <65987838+Nshandeshu@users.noreply.github.com>
2022-10-28 16:31:44 +0800

    Update 1.js

--- JS/fileball/1.js
+++ JS/fileball/1.js
@@ -8,7 +8,7 @@
 
 [rewrite_local]
 
-^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/fileball.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/Nshandeshu/OX/main/JS/fileball/1.js
 
 ^https:\/\/app-measurement\.com\/config\/app\/1?(.*?)*$ url reject
 
