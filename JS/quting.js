ba4085c8464bf96e3da7d1ad4de1a724aa4f0511
Nshandeshu <65987838+Nshandeshu@users.noreply.github.com>
2022-10-30 22:19:22 +0800

    Update quting.js

--- JS/quting.js
+++ JS/quting.js
@@ -2,6 +2,7 @@
 @@Zoo
 趣听音乐屏蔽升级弹窗
 日期:2022.10.30
+# https://github.com/Crazy-Z7/Scrip/blob/main/quting.js
 [rewrite_local]
 ^https:\/\/api\.bspapp\.com\/client url reject-dict
 hostname = api.bspapp.com
