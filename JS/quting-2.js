07983563a977223533aaaa239383d95fe4af323e
Nshandeshu <65987838+Nshandeshu@users.noreply.github.com>
2022-10-31 09:35:21 +0800

    趣听-2

---
+++ JS/quting-2.js
@@ -0,0 +1,27 @@
+/***********************************
+
+> 应用名称：趣听音乐
+> 下载地址：https://apps.apple.com/cn/app/id1640507964
+> 变身步骤：进入APP，输入“趣听音乐馆”
+> 脚本功能：免输入激活码，自动激活APP，免弹窗
+> 脚本作者：Cuttlefish
+> 微信账号：墨鱼手记
+> 更新时间：2022-10-31
+
+            
+[rewrite_local]
+
+# ～ 趣听音乐☆自动激活APP（2022-10-26）@ddgksf2013
+^https?:\/\/api\.bspapp\.com\/client$ url script-response-body https://gitlab.com/ddgksf2013/Cuttlefish/-/raw/master/Crack/qtmusic.js
+
+[mitm]
+
+hostname=api.bspapp.com
+
+***********************************/
+
+
+
+
+var body = $response.body.replace(/needVerify":true/g, 'needVerify":false').replace(/code":\d+/g, 'code":0');
+$done({ body });
