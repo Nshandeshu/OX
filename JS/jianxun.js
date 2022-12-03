4855f9dc23eda9b24c352880deb6bb1953f850af
Nshandeshu <65987838+Nshandeshu@users.noreply.github.com>
2022-12-03 17:11:11 +0800

    Update jianxun.js

--- JS/jianxun.js
+++ JS/jianxun.js
@@ -2,7 +2,7 @@
 
 脚本功能：简讯——解锁VIP
 下载地址：https://is.gd/bs4iRX
-软件版本：5.0.1
+软件版本：5.0.1 无暗黑模式，去广告需另加 >>>>>> 墨鱼版本VIP+去广告+暗黑模式：https://github.com/Nshandeshu/OX/blob/main/JS/JianXun2-moyu.js
 脚本作者：彭于晏💞
 使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
