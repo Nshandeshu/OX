e57571ae2df8ee60b5b6f630892873d9b12984b6
Nshandeshu <65987838+Nshandeshu@users.noreply.github.com>
2022-12-08 18:11:37 +0800

    1.2.8

--- JS/fileball/2.js
+++ JS/fileball/2.js
@@ -12,9 +12,9 @@
 
 hostname = *.googleapis.com
 
-projects = type=http-response,pattern=^https://firebaseremoteconfig.googleapis.com/v1/projects/filebox-ac299/.*,requires-body=1,max-size=0,script-path=https://Nshandeshu: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raw.githubusercontent.com/Nshandeshu/OX/main/JS/fileball/1.2.8-JS-yinyong/projects.js
+projects = type=http-response,pattern=^https://firebaseremoteconfig.googleapis.com/v1/projects/filebox-ac299/.*,requires-body=1,max-size=0,script-path=https://Nshandeshu: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@raw.githubusercontent.com/Nshandeshu/OX/main/JS/fileball/1.2.8-JS-yinyong/projects.js
 
-fileball = type=http-response,pattern=^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$,requires-body=1,max-size=0,script-path=https://Nshandeshu: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raw.githubusercontent.com/Nshandeshu/OX/main/JS/fileball/1.2.8-JS-yinyong/fileball.js
+fileball = type=http-response,pattern=^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$,requires-body=1,max-size=0,script-path=https://Nshandeshu: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@raw.githubusercontent.com/Nshandeshu/OX/main/JS/fileball/1.2.8-JS-yinyong/fileball.js
 
 
 
