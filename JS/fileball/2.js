6e00cc465b3f5d553b91c98bafdf3c7d1ec6b9c9
Nshandeshu <65987838+Nshandeshu@users.noreply.github.com>
2022-12-08 18:03:32 +0800

    Delete 2.js

--- JS/fileball/2.js
+++
@@ -1,4 +0,0 @@
-hostname = *.googleapis.com,api.revenuecat.com, app-measurement.com
-
-projects = type=http-response,pattern=^https://firebaseremoteconfig.googleapis.com/v1/projects/filebox-ac299/.*,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Alex0510/Eric/master/surge/Script/projects.js
-fileball = type=http-response,pattern=^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/fileball.js
